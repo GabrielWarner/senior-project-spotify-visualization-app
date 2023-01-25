86cd4492c3b331737e8ff8ba4b6f6c88554f0d40
GabrielWarner <gabe0412@uw.edu>
2023-01-25 15:30:40 -0800

    added chart for top 10 artist

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./assets/components/Header.jsx";
 import NowPlaying from "./assets/components/NowPlaying";
 import Home from "./assets/components/Home";
 import Analysis from "./assets/components/Analysis";
+import TopArtist from "./assets/components/TopArtist";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-js";
 
@@ -23,13 +24,16 @@ const getTokenFromUrl = () => {
 
 function App() {
   const [spotifyToken, setSpotifyToken] = useState("");
-  const [nowPlaying, setNowPlaying] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
   const [currentPage, setCurrentPage] = useState("Home");
-  //analysis page
-  const [searchQuery, setSearchQuery] = useState('');
-  const [trackId, setTrackId] = useState('');
+  //ANALYSIS page states
+  const [searchQuery, setSearchQuery] = useState("");
+  const [trackId, setTrackId] = useState("");
   const [audioAnalysis, setAudioAnalysis] = useState({});
+  //CURRENT TRACK states
+  const [nowPlaying, setNowPlaying] = useState({});
+  //TOP ARTISTS states
+  const [topArtists, setTopArtists] = useState({});
 
   useEffect(() => {
     const spotifyToken = getTokenFromUrl().access_token;
@@ -40,17 +44,45 @@ function App() {
       setSpotifyToken(spotifyToken);
       spotifyApi.setAccessToken(spotifyToken);
       setLoggedIn(true);
+      spotifyApi
+        .getMyTopArtists({ limit: 10 })
+        .then((data) => {
+          console.log(data);
+          setTopArtists(data);
+        })
+        .catch((error) => console.error(error));
     }
   });
 
   useEffect(() => {
-    console.log("AA TRACKID")
     if (trackId) {
-      spotifyApi.getAudioAnalysisForTrack(trackId)
-        .then(data => setAudioAnalysis(data))
-        .catch(error => console.error(error));
+      spotifyApi
+        .getAudioAnalysisForTrack(trackId)
+        .then((data) => setAudioAnalysis(data))
+        .catch((error) => console.error(error));
     }
   }, [trackId]);
+
+  async function handleSearch(event) {
+    event.preventDefault();
+    try {
+      const searchResults = await spotifyApi.searchTracks(searchQuery);
+      const firstTrackId = searchResults.tracks.items[0].id;
+      setTrackId(firstTrackId);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log(response);
+      setNowPlaying({
+        title: response.item.name,
+        albumArt: response.item.album.images[0].url,
+      });
+    });
+  };
 
   const renderPage = () => {
     if (currentPage === "Home") {
@@ -64,35 +96,29 @@ function App() {
     }
 
     if (currentPage === "Analysis") {
-      return <Analysis handleSearch={handleSearch} searchQuery={searchQuery} setSearchQuery={setSearchQuery} trackId={trackId} audioAnalysis={audioAnalysis}/>;
+      return (
+        <Analysis
+          handleSearch={handleSearch}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          trackId={trackId}
+          audioAnalysis={audioAnalysis}
+        />
+      );
+    }
+
+    if (currentPage === "TopArtist") {
+      return <TopArtist topArtists={topArtists} />;
     }
   };
-
-  const getNowPlaying = () => {
-    spotifyApi.getMyCurrentPlaybackState().then((response) => {
-      console.log(response);
-      setNowPlaying({
-        title: response.item.name,
-        albumArt: response.item.album.images[0].url,
-      });
-    });
-  };
-
-  async function handleSearch(event) {
-    event.preventDefault();
-    try {
-      const searchResults = await spotifyApi.searchTracks(searchQuery);
-      const firstTrackId = searchResults.tracks.items[0].id;
-      setTrackId(firstTrackId);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
 
   return (
     <div className="App">
-      <Header loggedIn={loggedIn} setCurrentPage={setCurrentPage}></Header>
+      <Header
+        loggedIn={loggedIn}
+        setCurrentPage={setCurrentPage}
+        topArtists={topArtists}
+      ></Header>
       {renderPage()}
     </div>
   );
