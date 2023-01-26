5e0cf4321f5579c0e555f25d76d2cdbb0c231f97
GabrielWarner <gabe0412@uw.edu>
2023-01-25 16:33:06 -0800

    changes to graph

--- front-end/src/assets/components/TopArtist.jsx
+++ front-end/src/assets/components/TopArtist.jsx
@@ -1,45 +1,73 @@
 import React, { useState, useEffect } from "react";
 import {
-    Chart as ChartJS,
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    Tooltip,
-    Legend
-} from 'chart.js'
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import zoomPlugin from "chartjs-plugin-zoom";
 import "../styles/topArtist.css";
-import { Bar } from 'react-chartjs-2'
+import { Bar } from "react-chartjs-2";
 ChartJS.register(
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    Tooltip,
-    Legend
-)
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+  zoomPlugin
+);
 
-function TopArtist({topArtists}) {
+function TopArtist({ topArtists }) {
+  const data = {
+    labels: topArtists.items.map(
+      (artist, index) => `${index + 1}:${artist.name} (${artist.popularity})`
+    ),
+    datasets: [
+      {
+        label: "Followers",
+        data: topArtists.items.map((artist) => artist.followers.total),
+        backgroundColor: "rgba(29, 185, 84, 0.2)",
+        borderColor: "rgba(29, 185, 84, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    const data = {
-        labels: topArtists.items.map((artist, index) => `${index+1}:${artist.name} (${artist.popularity})`),
-        datasets: [
-          {
-            label: 'Followers',
-            data: topArtists.items.map(artist => artist.followers.total),
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
+  const options = {
+    indexAxis: "y",
+    x: {
+
+    },
+    plugins: {
+    zoom: {
+        zoom: {
+            wheel: {
+              enabled: true,
+            },
+            pinch: {
+              enabled: false,
+            },
+            mode: "x",
+            drag: {
+              enabled: false,
+            },
           },
-        ]
-      };
-    
-    const options = {
-
+          limits: {
+            x: {min: 'original', max: 'original', minRange: 100000},
+          },
+          pan: {
+            enabled: true,
+          }
     }
+    },
+  };
 
-    return (
-        <div>
-        <Bar data={data} options={options}/>
-      </div>
+  return (
+    <div>
+      <Bar data={data} options={options} />
+    </div>
   );
 }
 export default TopArtist;
