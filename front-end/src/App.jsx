19e9d0f71da227e2437f57209ad72b79b20b6a85
GabrielWarner <gabe0412@uw.edu>
2023-02-08 23:10:24 -0800

    updates

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,11 +8,11 @@ import Analysis from "./assets/components/Analysis";
 import TopArtist from "./assets/components/TopArtist";
 import TopTracks from "./assets/components/TopTracks";
 import LastFiftySaved from "./assets/components/LastFiftySaved";
-import Top10 from "./assets/components/Top10"
+import Top10 from "./assets/components/Top10";
 // import SavedTracks from "./assets/components/SavedTracks";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-js";
-import topTenArtists from "./data/topTenArtists.json"
+import topTenArtists from "./data/topTenArtists.json";
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -35,6 +35,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [trackId, setTrackId] = useState("");
   const [audioAnalysis, setAudioAnalysis] = useState({});
+  const [audioFeatures, setAudioFeatures] = useState({});
   const [track, setTrack] = useState({});
   //CURRENT TRACK states
   const [nowPlaying, setNowPlaying] = useState({});
@@ -43,7 +44,7 @@ function App() {
   //TOP TRACKS states
   const [topTracks, setTopTracks] = useState({});
   //LAST 50 SAVED TRACKS
-  const [savedTracks, setSavedTracks] = useState({})
+  const [savedTracks, setSavedTracks] = useState({});
   //TOP 10 PAGE
 
   useEffect(() => {
@@ -53,43 +54,45 @@ function App() {
       async function getSavedTracks() {
         const result = await spotifyApi.getMySavedTracks({ limit: 50 });
         setSavedTracks(result.items);
-        console.log(result.items)
+        console.log(result.items);
       }
       setSpotifyToken(spotifyToken);
       spotifyApi.setAccessToken(spotifyToken);
       setLoggedIn(true);
       spotifyApi
-        .getMyTopArtists({ limit: 10, time_range:"long_term", })
+        .getMyTopArtists({ limit: 10, time_range: "long_term" })
         .then((data) => {
           setTopArtists(data);
         })
         .catch((error) => console.error(error));
       spotifyApi
-        .getMyTopTracks({ limit: 10, time_range:"long_term", })
+        .getMyTopTracks({ limit: 10, time_range: "long_term" })
         .then((data) => {
-          setTopTracks(data)
-        })
-      getSavedTracks()
-      saveData()
+          setTopTracks(data);
+        });
+      getSavedTracks();
+      saveData();
     }
   });
 
-
   async function saveData() {
     // Make a request to the Spotify API
-    const response = await spotifyApi.getAlbum('7ycBtnsMtyVbbwTfJwRjSP');
-  
+    const response = await spotifyApi.getAlbum("7ycBtnsMtyVbbwTfJwRjSP");
+
     // Save the data to the local storage
-    localStorage.setItem('albumData', JSON.stringify(response));
+    localStorage.setItem("albumData", JSON.stringify(response));
   }
 
   useEffect(() => {
-      spotifyApi
-        .getAudioFeaturesForTrack(trackId)
-        .then((data) => (setAudioAnalysis(data), console.log(data)))
-        .catch((error) => console.error(error));
+    spotifyApi
+      .getAudioFeaturesForTrack(trackId)
+      .then((data) => (setAudioFeatures(data), console.log(data)))
+      .catch((error) => console.error(error));
+    spotifyApi
+      .getAudioAnalysisForTrack(trackId)
+      .then((data) => (setAudioAnalysis(data), console.log(data)))
+      .catch((error) => console.error(error));
   }, [trackId]);
-
 
   async function handleSearch(event) {
     event.preventDefault();
@@ -97,9 +100,8 @@ function App() {
       const searchResults = await spotifyApi.searchTracks(searchQuery);
       const firstTrackId = searchResults.tracks.items[0].id;
       setTrackId(firstTrackId);
-      const track = searchResults.tracks.items[0]
-      setTrack(track)
-      console.log(track)
+      const track = searchResults.tracks.items[0];
+      setTrack(track);
       // const getAudioFeaturesForTrack = await spotifyApi.getAudioFeaturesForTrack(trackId)
       // setAudioAnalysis(getAudioFeaturesForTrack)
     } catch (error) {
@@ -137,6 +139,7 @@ function App() {
           trackId={trackId}
           audioAnalysis={audioAnalysis}
           track={track}
+          audioFeatures={audioFeatures}
         />
       );
     }
@@ -150,11 +153,11 @@ function App() {
     }
 
     if (currentPage === "LastFiftySaved") {
-      return <LastFiftySaved savedTracks={savedTracks}/>;
+      return <LastFiftySaved savedTracks={savedTracks} />;
     }
 
     if (currentPage === "Top10") {
-      return <Top10/>;
+      return <Top10 />;
     }
   };
 
--- front-end/src/assets/components/Analysis.jsx
+++ front-end/src/assets/components/Analysis.jsx
@@ -1,5 +1,5 @@
 import '../styles/analysis.css';
-function Analysis({handleSearch, searchQuery, setSearchQuery, trackId, audioAnalysis, track}) {
+function Analysis({handleSearch, searchQuery, setSearchQuery, trackId, audioAnalysis, audioFeatures, track}) {
 
   return (
     <>
@@ -16,69 +16,69 @@ function Analysis({handleSearch, searchQuery, setSearchQuery, trackId, audioAnal
       {track && track.name && <h2>Track name: {track.name}</h2>}
       {track && track.artists && track.artists[0] && track.artists[0].name && <h3>Artist: {track.artists[0].name}</h3>}
       {track && track.album && track.album.images && track.album.images[1] && track.album.images[1].url && <img src={track.album.images[1].url} alt="Track Image"></img>}
-      {audioAnalysis && (
+      {audioFeatures && (
         <div>
           <p>Audio Features:</p>
           <div className="analysis-container">
 
             <div className="analysis-feature">
               <p className="feature-name">Danceability:</p>
-              <p className="feature-value">{audioAnalysis.danceability}</p>
+              <p className="feature-value">{audioFeatures.danceability}</p>
               <p>A value from 0.0 to 1.0 that describes how suitable a track is for dancing, based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Energy:</p>
-              <p className="feature-value">{audioAnalysis.energy}</p>
+              <p className="feature-value">{audioFeatures.energy}</p>
               <p>A value from 0.0 to 1.0 that represents a perceptual measure of intensity and activity, from relaxed and calm to energetic and loud.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Key:</p>
-              <p className="feature-value">{audioAnalysis.key}</p>
+              <p className="feature-value">{audioFeatures.key}</p>
               <p>The estimated overall key of the track. Integers map to pitches using standard Pitch Class notation.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Loudness:</p>
-              <p className="feature-value">{audioAnalysis.loudness}</p>
+              <p className="feature-value">{audioFeatures.loudness}</p>
               <p>The overall loudness of a track in decibels (dB).</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Mode:</p>
-              <p className="feature-value">{audioAnalysis.mode}</p>
+              <p className="feature-value">{audioFeatures.mode}</p>
               <p>The modality (major or minor) of a track, the type of scale from which its melodic content is derived.</p>
 
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Speechiness:</p>
-              <p className="feature-value">{audioAnalysis.speechiness}</p>
+              <p className="feature-value">{audioFeatures.speechiness}</p>
               <p>A value from 0.0 to 1.0 that represents the presence of spoken words in a track.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Acousticness:</p>
-              <p className="feature-value">{audioAnalysis.acousticness}</p>
+              <p className="feature-value">{audioFeatures.acousticness}</p>
               <p>A value from 0.0 to 1.0 that describes how acoustic a track is, with 1.0 being the most acoustic.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Instrumentalness:</p>
-              <p className="feature-value">{audioAnalysis.instrumentalness}</p>
+              <p className="feature-value">{audioFeatures.instrumentalness}</p>
               <p>A value from 0.0 to 1.0 that describes the likelihood that the track contains no vocals.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Liveness:</p>
-              <p className="feature-value">{audioAnalysis.liveness}</p>
+              <p className="feature-value">{audioFeatures.liveness}</p>
               <p>A value from 0.0 to 1.0 that represents the likelihood that the track was performed live.</p>
             </div>
 
             <div className="analysis-feature">
               <p className="feature-name">Valence:</p>
-              <p className="feature-value">{audioAnalysis.valence}</p>
+              <p className="feature-value">{audioFeatures.valence}</p>
               <p>A value from 0.0 to 1.0 that represents the musical positiveness conveyed by a track, with 1.0 being the most positive.</p>
             </div>
           </div>
