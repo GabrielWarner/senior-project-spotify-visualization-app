66049d0aca8fdb74a5760f4b600826cebdbd2de0
GabrielWarner <gabe0412@uw.edu>
2023-02-15 14:26:45 -0800

    updates

--- front-end/src/assets/components/Top10.jsx
+++ front-end/src/assets/components/Top10.jsx
@@ -22,13 +22,31 @@ function Top10() {
       labels: artistsData.map((artist) => artist.name),
       datasets: [
         {
-          label: "Genres",
+          label: "Genre Count:",
           data: artistsData.map((artist) => artist.genres.length),
           backgroundColor: ["rgba(29, 185, 84, .2)"],
           borderColor: ["rgba(29, 185, 84, 1)"],
           borderWidth: 1,
         },
       ],
+      tooltips: {
+            callbacks: {
+              label: function (tooltipItem, data) {
+                const artistIndex = tooltipItem.index;
+                const artist = artistsData[artistIndex];
+                if (selectedGraph === "genres") {
+                  const genresList = artist.genres
+                    .map((genre) => `- ${genre}`)
+                    .join("\n");
+                  return `All Genres:\n${genresList}`;
+                } else if (selectedGraph === "followers") {
+                  return `${artist.name}: ${artist.followers}`;
+                } else {
+                  return `${artist.name}: #${artist.popularity}`;
+                }
+              },
+            },
+          },
     },
     followers: {
       labels: artistsData.map((artist) => artist.name),
@@ -66,8 +84,8 @@ function Top10() {
           id="graph-select"
           onChange={(e) => setSelectedGraph(e.target.value)}
         >
-          <option value="genres">All Genres</option>
           <option value="followers">Followers</option>
+          <option value="genres">All Genres</option>
           <option value="rank">Ranking</option>
         </select>
       </div>
