47f101b9a2094b55520d13c7b6c229e3e92920c4
GabrielWarner <gabe0412@uw.edu>
2023-03-01 15:11:16 -0800

    added genres to top 10 page

--- front-end/src/assets/components/Top10.jsx
+++ front-end/src/assets/components/Top10.jsx
@@ -1,5 +1,7 @@
 import topTenArtists from "../../data/topTenArtists.json";
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 import { Bar } from "react-chartjs-2";
 
 function Top10() {
@@ -76,6 +78,33 @@ function Top10() {
 
   const filteredData = data[selectedGraph];
 
+  const Table = styled(motion.table)`
+  margin: auto;
+  width: 50vw;
+  border-collapse: collapse;
+  margin-top: 2rem;
+
+  th {
+    text-align: center;
+    padding: 0.5rem;
+    border-bottom: 2px solid #ddd;
+  }
+
+  td {
+    padding: 0.5rem;
+    border-bottom: 1px solid #ddd;
+  }
+`;
+
+const headers = ["Artist Name", "Genre Types"];
+
+const tableData = artistsData.map((artist, index) => ({
+  artistName: '# ' + (index + 1) + ' ' + artist.name,
+  genreTypes: artist.genres.join(", "),
+}));
+
+
+
   return (
     <div className="container">
       <div>
@@ -118,6 +147,29 @@ function Top10() {
         }}
       />
       </div>
+
+      <Table
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  exit={{ opacity: 0 }}
+>
+  <thead>
+    <tr>
+      {headers.map((header) => (
+        <th key={header}>{header}</th>
+      ))}
+    </tr>
+  </thead>
+  <tbody>
+    {tableData.map((data) => (
+      <tr key={data.artistName}>
+        <td>{data.artistName}</td>
+        <td>{data.genreTypes}</td>
+      </tr>
+    ))}
+  </tbody>
+</Table>
+
     </div>
   );
 }
